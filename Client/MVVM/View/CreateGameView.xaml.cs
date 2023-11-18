1529504c01221a5fe67cb104ee7040212469ef46
StanislavAndroshchuk <newmegodvenm@gmail.com>
2023-11-18 21:15:30 +0200

    oops

---
+++ Client/MVVM/View/CreateGameView.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client.MVVM.View;
+
+public partial class CreateGameView : UserControl
+{
+    public CreateGameView()
+    {
+        InitializeComponent();
+    }
+
+    
+}
