65e79d84459d1962247293f1dda6952a7a1785e9
Den <90708440+dklymovych@users.noreply.github.com>
2023-11-23 02:31:53 +0200

    add join room

--- Server/Controllers/GameController.cs
+++ Server/Controllers/GameController.cs
@@ -6,6 +6,7 @@ using Server.Dto;
 using System.Security.Claims;
 using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace Server.Controllers;
 
@@ -27,20 +28,54 @@ public class GameController : ControllerBase
     [Authorize]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public ActionResult CreateRoom([FromBody] CreateRoomDto createRoomDtoDto)
+    public ActionResult CreateRoom([FromBody] CreateRoomDto createRoomDto)
     {
         Room room = new Room
         {
-            GameCode = createRoomDtoDto.GameCode,
-            IsPublic = createRoomDtoDto.IsPublic,
+            GameCode = createRoomDto.GameCode,
+            IsPublic = createRoomDto.IsPublic,
             Player1 = _currentPlayer,
-            ShipsPosition1 = JsonSerializer.Deserialize<ShipsPosition>(createRoomDtoDto.ShipsPosition)!
+            ShipsPosition1 = JsonSerializer.Deserialize<ShipsPosition>(createRoomDto.ShipsPosition)!
         };
 
         _context.Add(room);
         _context.SaveChanges();
 
         return StatusCode(StatusCodes.Status201Created);
+    }
+
+    [HttpPut("JoinRoom/{gameCode}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult JoinRoom([FromRoute] string gameCode, [FromBody] JoinRoomDto joinRoomDto)
+    {
+        Room? room = FindRoom(gameCode);
+
+        if (room == null)
+            return NotFound();
+
+        if (room.Player1 == _currentPlayer)
+            return BadRequest();
+        
+        if (room.Player2 != null)
+            return BadRequest();
+
+        room.Player2 = _currentPlayer;
+        room.GameStartedAt = DateTime.Now;
+        room.ShipsPosition2 = JsonSerializer.Deserialize<ShipsPosition>(joinRoomDto.ShipsPosition)!;
+
+        _context.SaveChanges();
+        return Ok();
+    }
+
+    private Room? FindRoom(string gameCode) {
+        return _context.Rooms
+            .Include(r => r.Player1)
+            .Include(r => r.Player2)
+            .Include(r => r.Winner)
+            .FirstOrDefault(r => r.GameCode == gameCode);
     }
 
     private Player _currentPlayer => (
