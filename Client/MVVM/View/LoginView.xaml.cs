454aab035ec138e25bf2d9e16fc6000b210f3fd4
StanislavAndroshchuk <newmegodvenm@gmail.com>
2023-11-24 12:32:14 +0200

    Register + Login

--- Client/MVVM/View/LoginView.xaml.cs
+++ Client/MVVM/View/LoginView.xaml.cs
@@ -11,34 +11,4 @@ public partial class LoginView : UserControl
     {
         InitializeComponent();
     }
-
-    private async void Button_Click(object sender, System.Windows.RoutedEventArgs e)
-    {
-        using (HttpClient httpClient = new HttpClient())
-        {
-
-            string username = LoginTextBox.Text;
-            string password = PasswordTextBox.Password;
-
-            string apiUrl = "http://localhost:5199";
-
-            var postData = new { username = username, password = password };
-
-            string jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(postData);
-            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-            HttpResponseMessage responsePost = await httpClient.PostAsync($"{apiUrl}/api/Auth/Login", content);
-
-            if (responsePost.IsSuccessStatusCode)
-            {
-                // Get the content of the response
-                string responseContent = await responsePost.Content.ReadAsStringAsync();
-                MessageBox.Show("POST Request Response: \n" + responseContent);
-            }
-            else
-            {
-                MessageBox.Show($"POST Request failed with status code {(int) responsePost.StatusCode}");
-            }
-        }
-    }
 }
--- Client/MVVM/ViewModel/LoginViewModel.cs
+++ Client/MVVM/ViewModel/LoginViewModel.cs
@@ -1,11 +1,43 @@
+using System;
+using System.Collections.Generic;
 using Client.Core;
 using Client.Services;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using Client.MVVM.Model;
+using Newtonsoft.Json.Linq;
 
 namespace Client.MVVM.ViewModel;
 
 public class LoginViewModel : Core.ViewModel
 {
+    private string _username;
+    public string Username
+    {
+        get { return _username; }
+        set
+        {
+            _username = value;
+            OnPropertyChanged("Username");
+        }
+    }
+
+    private string _password;
+    public string Password
+    {
+        get { return _password; }
+        set
+        {
+            _password = value;
+            OnPropertyChanged("Password");
+        }
+    }
+
     private INavigationService _navigation;
     
     public INavigationService Navigation
@@ -19,15 +51,63 @@ public class LoginViewModel : Core.ViewModel
     }
     public RelayCommand NavigateToHomeCommand { get; set; }
     public RelayCommand NavigateToRatingViewCommand { get; set; }
+    public ICommand SubmitCommand { get; set; }
+    private async void Submit()
+    {
+        using (HttpClient httpClient = new HttpClient())
+        {
+
+            string username = Username;
+            string password = Password;
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Fields are empty");
+                return;
+            }
+           
+
+            string apiUrl = "http://localhost:5199";  // This needs to be in file config
+
+            var postData = new { username = username, password = password };  // This needs to be in file config
+
+            string jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(postData);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage responsePost = await httpClient.PostAsync($"{apiUrl}/api/Auth/Login", content);
+
+            if (responsePost.IsSuccessStatusCode)
+            {
+                //To get json
+                var responseContent = await responsePost.Content.ReadAsStringAsync();
+                var token = JObject.Parse(responseContent)["accessToken"]!.ToString();
+                User user = new User(token);
+                Globals.LogginInUser = user;
+                Username = "";
+                Password = "";
+                Navigation.NavigateTo<RatingViewModel>();
+            }
+            else
+            {
+                if ((int)responsePost.StatusCode == 400 )
+                {
+                    MessageBox.Show("Bad request");
+                }
+                if ((int)responsePost.StatusCode == 401)
+                {
+                    MessageBox.Show("Username or password are incorrect!");
+                }
+            }
+           
+        }
+    }
+
+    public RelayCommand NavigateToRegisterViewCommand { get; set; }
 
     public LoginViewModel(INavigationService navigation)
     {
-        
         Navigation = navigation;
-        //NavigateToLoginViewCommand = new RelayCommand(o =>{Navigation.NavigateTo<LoginViewModel>();}, canExecute:o => true)
-        NavigateToHomeCommand = new RelayCommand(o => { Navigation.NavigateTo<HomeViewModel>();}, canExecute:o => true );
         NavigateToRatingViewCommand = new RelayCommand(o => { Navigation.NavigateTo<RatingViewModel>();}, canExecute:o => true );
-
-        //NavigateToSettingsViewCommand = new RelayCommand(o => { Navigation.NavigateTo<SettingsViewModel>();}, canExecute:o => true );
+        NavigateToRegisterViewCommand = new RelayCommand(o => { Navigation.NavigateTo<RegisterViewModel>();}, canExecute:o => true );
+        SubmitCommand = new RelayCommand(o => { Submit(); }, canExecute:o => true); 
     }
 }
