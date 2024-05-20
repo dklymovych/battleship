b1565c5bd1ac2e9c6122b001267cd217244c3850
BroodCaster <BroodCaster@gmail.com>
2024-05-20 19:09:05 +0300

    done

--- WebApp/Pages/Scoreboard/Index.cshtml.cs
+++ WebApp/Pages/Scoreboard/Index.cshtml.cs
@@ -8,32 +8,24 @@ namespace WebApp.Pages
     {
         private readonly GameApiService _gameApiService;
 
+        public ScoreboardDto Scoreboard { get; set; }
+
         public ScoreboardModel(GameApiService gameApiService)
         {
             _gameApiService = gameApiService;
         }
 
-        public ScoreboardDto Scoreboard { get; set; } = new ScoreboardDto();
-
         public async Task OnGetAsync()
         {
             var response = await _gameApiService.Scoreboard();
+
             if (response.StatusCode == System.Net.HttpStatusCode.OK && response.Data != null)
             {
                 Scoreboard = response.Data;
             }
             else
             {
-                Scoreboard = new ScoreboardDto
-                {
-                    scoreboard = new[]
-                    {
-                        //діставати з баз даних
-                        new ScoreboardItemDto { username = "User1", winRate = 75, numberOfGames = 20 },
-                        new ScoreboardItemDto { username = "User2", winRate = 60, numberOfGames = 15 },
-                        new ScoreboardItemDto { username = "User3", winRate = 85, numberOfGames = 25 },
-                    }
-                };
+                Scoreboard = new ScoreboardDto { scoreboard = Array.Empty<ScoreboardItemDto>() };
             }
         }
     }
