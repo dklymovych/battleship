49ca9b2025d2e6d93a67f2fe3d8562d84462f4e0
BroodCaster <BroodCaster@gmail.com>
2024-05-20 16:35:09 +0300

    add login page

---
+++ WebApp/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Dto;
+using WebApp.Services;
+
+namespace WebApp.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly AuthApiService _authApiService;
+
+        public LoginModel(AuthApiService authApiService)
+        {
+            _authApiService = authApiService;
+            Player = new PlayerDto();
+            ErrorMessage = string.Empty;
+        }
+
+        [BindProperty]
+        public PlayerDto Player { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var response = await _authApiService.Login(Player);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var accessToken = response.Data?.accessToken;
+                var refreshToken = response.Data?.refreshToken;
+
+                if (accessToken != null && refreshToken != null)
+                {
+                    Console.WriteLine("Access Token: " + accessToken);
+                    Console.WriteLine("Refresh Token: " + refreshToken);
+                    var script = $"<script>localStorage.setItem('accessToken', '{accessToken}'); localStorage.setItem('refreshToken', '{refreshToken}');</script>";
+                    HttpContext.Response.Headers.Append("X-LocalStorage-Script", script);
+                }
+
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                ErrorMessage = "Invalid username or password";
+                return Page();
+            }
+        }
+    }
+}
